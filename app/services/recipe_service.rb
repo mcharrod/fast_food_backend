0ef171859418e54e26067cd0913672ba1e375862
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-02 09:30:49 -0600

    Add search by id lookup

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,18 +1,23 @@
-
-
 class RecipeService
   def self.search_for_meal_by_name(meal_name)
     response = conn.get('/api/json/v1/1/search.php') do |faraday|
-      faraday.params['s'] = "spaghetti"
+      faraday.params['s'] = meal_name
       # faraday.params['api_key'] = 1
     end
     parsed = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
   end
 
   def self.conn
     Faraday.new(url: 'https://themealdb.com') do |f|
       f.adapter Faraday.default_adapter
     end
+  end
+
+
+  def self.search_meal_by_id(meal_id)
+    response = conn.get('/api/json/v1/1/lookup.php') do |faraday|
+      faraday.params['i'] = meal_id
+    end
+    parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/meal_service_spec.rb
+++ spec/services/meal_service_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
-describe 'something' do
-  it 'does thing' do
+describe 'recipe api service' do
+  it 'searches by name of meal' do
     response = RecipeService.search_for_meal_by_name("Spaghetti")
 
     expect(response[:meals]).to be_an(Array)
     expect(response[:meals].first[:strMeal]).to be_a(String)
     expect(response[:meals].first[:strInstructions].first[:strMeal]).to be_a(String)
+  end
 
-    # expect status to be 200
-    # expect a string to be a string etc
+  it 'searches by id of meal' do
+    response = RecipeService.search_meal_by_id('52772')
+    meal = response[:meals].first
+
+    expect(meal[:idMeal]).to eq('52772')
   end
 end
