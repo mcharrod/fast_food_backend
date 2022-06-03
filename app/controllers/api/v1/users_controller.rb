139a435d847d8bb98aa45bd4696ae4fc9e51fc76
reuben <creubend@gmail.com>
2022-06-03 15:06:25 -0600

    feat: User is created from frontend api call

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::UsersController <ApplicationController 
+    skip_before_action :verify_authenticity_token
     def show 
 
     end
 
     def create
-        user = User.find_create_by(params[:email])
+        user = User.find_or_create_by(name: params[:name], email: params[:email])
     end
 end
