8233a70cd7987ae5c07b8abc34f9bd67243305b4
reuben <creubend@gmail.com>
2022-06-03 15:38:05 -0600

    feat: created an update method for a user

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'figaro'
 gem 'faraday'
+gem 'jsonapi-serializer'
 
 
 group :test do
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,26 @@
 class Api::V1::UsersController <ApplicationController 
     skip_before_action :verify_authenticity_token
     def show 
-
+        user = User.find_by(email: params[:email])
+        render json: UserSerializer.new(user)
     end
 
     def create
         user = User.find_or_create_by(name: params[:name], email: params[:email])
+        render json: UserSerializer.new(user)
+    end
+
+    def update 
+        user = User.update(:email, user_params)
+        if user.save 
+            render json: UserSerializer.new(user) 
+        else 
+            render status: 400
+        end
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:name, :email)
     end
 end
