c039644fe969c09cdf7295978a36df75b2386d30
reuben <creubend@gmail.com>
2022-06-03 08:55:40 -0600

    feat: Route created to find a user by their email

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # namespace :recipes do
         get 'recipes/search', to: 'recipes#name_find'
         get 'recipes/find', to: 'recipes#id_find'
         get 'recipes/random_meal', to: 'recipes#random_find'
         get 'recipes/ingredient', to: 'recipes#ingredient_find'
         get 'recipes/category', to: 'recipes#category_find'
         get 'recipes/area', to: 'recipes#area_find'
-      # end
-      resources :users, except:[:index]
+      resources :users, except:[:index, :show]
+      get 'user', to: 'users#show'
+
     end
   end
 end
--- spec/requests/api/v1/users/user_request_spec.rb
+++ spec/requests/api/v1/users/user_request_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "User CRUD funtionality" do 
-    it "can retrieve a specific user " do  
+    it "can retrieve a specific user " do 
+        create_list(:user, 10)
+        create(:user, email: "rod@gmail.com")
+        get '/api/v1/user?email=rod@gmail.com'
     end 
 end
