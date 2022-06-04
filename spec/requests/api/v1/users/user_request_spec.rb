01401bed663550e97f8d127989b590cf22856b4b
reuben <creubend@gmail.com>
2022-06-04 13:02:30 -0600

    test: a user can be created and returned when asked to find by an email

--- spec/requests/api/v1/users/user_request_spec.rb
+++ spec/requests/api/v1/users/user_request_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe "User CRUD funtionality" do
         expect(user[:data][:attributes]).to be_a(Hash)
         expect(user[:data][:attributes][:email]).to eq("rod@gmail.com")
     end 
+
+    it "can create or find a user by a post call" do 
+        create_list(:user, 10)
+        post '/api/v1/users?email=beth@gmail.com'
+        expect(User.count).to eq(11)
+        expect(response).to be_successful
+        user = JSON.parse(response.body, symbolize_names: true)
+        expect(user[:data][:attributes][:email]).to eq("beth@gmail.com")
+    end
 end
