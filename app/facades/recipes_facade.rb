1a75fbf7f23a1b32f82004042a90f7c1c8042231
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-05 01:44:13 -0600

    Facade refactor: poro enumerable maker in a helper method and alter method names

--- app/facades/recipes_facade.rb
+++ app/facades/recipes_facade.rb
@@ -1,38 +1,43 @@
 class RecipesFacade
   def self.find_recipes_by_name(recipe_name)
     recipes = RecipeService.search_recipes_by_name(recipe_name)
-    recipes[:meals].map do |recipe|
-      Recipe.new(recipe)
-    end
-  end
 
-  def self.find_recipe_by_id(recipe_id)
-    recipe = RecipeService.search_recipe_by_id(recipe_id)
-      Recipe.new(recipe[:meals].first)
-  end
-
-  def self.find_random_recipe
-    recipe = RecipeService.search_random_recipe
-      Recipe.new(recipe[:meals].first)
-  end
-
-  def self.find_recipes_by_ingredient(ingredient)
-    recipes = RecipeService.search_recipes_by_ingredient(ingredient)
-    recipes[:meals].map do |recipe|
-      Recipe.new(recipe)
-    end
-  end
-
-  def self.find_recipes_by_category(category)
-    recipes = RecipeService.search_recipes_by_category(category)
-    recipes[:meals].map do |recipe|
-      Recipe.new(recipe)
-    end
+    make_some_poros(recipes)
   end
 
   def self.find_recipes_by_area(area)
     recipes = RecipeService.search_recipes_by_area(area)
-    recipes[:meals].map do |recipe|
+
+    make_some_poros(recipes)
+  end
+
+  def self.find_recipes_by_ingredient(ingredient)
+    recipes = RecipeService.search_recipes_by_ingredient(ingredient)
+
+    make_some_poros(recipes)
+  end
+
+  def self.find_recipes_by_category(category)
+    recipes = RecipeService.search_recipes_by_category(category)
+
+    make_some_poros(recipes)
+  end
+
+  def self.find_recipe_by_id(recipe_id)
+    recipe = RecipeService.search_recipe_by_id(recipe_id)[:meals].first
+
+    Recipe.new(recipe)
+  end
+
+
+  def self.find_random_recipe
+    recipe = RecipeService.search_random_recipe[:meals].first
+
+    Recipe.new(recipe)
+  end
+
+  def self.make_some_poros(data)
+    data[:meals].map do |recipe|
       Recipe.new(recipe)
     end
   end
