2ce407b87a7a35382a087645c5a78ad85fe826bf
reuben <creubend@gmail.com>
2022-06-06 07:51:07 -0600

    test: a user can have many recipes

--- spec/models/saved_recipe_spec.rb
+++ spec/models/saved_recipe_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe SavedRecipe, type: :model do
     expect(user.saved_recipes.count).to eq 1
     expect(recipe.user_id).to eq(user.id)
   end
+
+  it 'user can have many' do 
+    user = create(:user)
+    create_list(:saved_recipe, 10, user_id: user.id)
+    expect(user.saved_recipes.count).to eq(10)
+  end
 end
