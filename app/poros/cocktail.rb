fad11c38cefff4acf5c156ef659842f1e88a582c
James Harkins <jh.harkins@protonmail.com>
2022-06-08 11:14:52 -0600

    Feat: pass tests for Cocktail POROs

--- app/poros/cocktail.rb
+++ app/poros/cocktail.rb
@@ -8,4 +8,20 @@ class Cocktail
     @ingredients = get_ingredients(cocktail_data)
     @instructions = cocktail_data[:strInstructions]
   end
+
+  def get_ingredients(cocktail_data)
+    ingredient_values = []
+    cocktail_data.keys.select do |key|
+      if key.to_s.include?("strIngredient")
+        ingredient_values.push(cocktail_data[key])
+      end
+    end
+    measurement_values = []
+    cocktail_data.keys.select do |key|
+      if key.to_s.include?("strMeasure")
+        measurement_values.push(cocktail_data[key])
+      end
+    end
+    ingredient_values.zip(measurement_values).to_h.compact
+  end
 end
--- spec/poros/cocktail_spec.rb
+++ spec/poros/cocktail_spec.rb
@@ -12,47 +12,45 @@ describe Cocktail do
       strIngredient2: "Angostura bitters",
       strIngredient3: "Sugar",
       strIngredient4: "Water",
-      strIngredient5: null,
-      strIngredient6: null,
-      strIngredient7: null,
-      strIngredient8: null,
-      strIngredient9: null,
-      strIngredient10: null,
-      strIngredient11: null,
-      strIngredient12: null,
-      strIngredient13: null,
-      strIngredient14: null,
-      strIngredient15: null,
+      strIngredient5: nil,
+      strIngredient6: nil,
+      strIngredient7: nil,
+      strIngredient8: nil,
+      strIngredient9: nil,
+      strIngredient10: nil,
+      strIngredient11: nil,
+      strIngredient12: nil,
+      strIngredient13: nil,
+      strIngredient14: nil,
+      strIngredient15: nil,
       strMeasure1: "4.5 cL",
       strMeasure2: "2 dashes",
       strMeasure3: "1 cube",
       strMeasure4: "dash",
-      strMeasure5: null,
-      strMeasure6: null,
-      strMeasure7: null,
-      strMeasure8: null,
-      strMeasure9: null,
-      strMeasure10: null,
-      strMeasure11: null,
-      strMeasure12: null,
-      strMeasure13: null,
-      strMeasure14: null,
-      strMeasure15: null
+      strMeasure5: nil,
+      strMeasure6: nil,
+      strMeasure7: nil,
+      strMeasure8: nil,
+      strMeasure9: nil,
+      strMeasure10: nil,
+      strMeasure11: nil,
+      strMeasure12: nil,
+      strMeasure13: nil,
+      strMeasure14: nil,
+      strMeasure15: nil
     }
 
     old_fashioned = Cocktail.new(attributes)
-
-    ingredients = {
-      "Bourbon" => "4.5 cL",
-      "Angostura bitters" => "2 dashes",
-      "Sugar" => "1 cube",
-      "Water" => "dash"
-    }
 
     expect(old_fashioned.id).to eq("11001")
     expect(old_fashioned.name).to eq("Old Fashioned")
     expect(old_fashioned.image).to eq("https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg")
     expect(old_fashioned.instructions).to eq("Place sugar cube in old fashioned glass and saturate with bitters, add a dash of plain water. Muddle until dissolved.\r\nFill the glass with ice cubes and add whiskey.\r\n\r\nGarnish with orange twist, and a cocktail cherry.")
-    expect(old_fashioned.ingredients).to eq(ingredients)
+    expect(old_fashioned.ingredients).to eq({
+      "Bourbon" => "4.5 cL",
+      "Angostura bitters" => "2 dashes",
+      "Sugar" => "1 cube",
+      "Water" => "dash"
+    })
   end
 end
