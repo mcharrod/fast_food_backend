af1a6a128c68b78fc4d208711b19ca72b93a70ff
reuben <creubend@gmail.com>
2022-06-09 09:46:49 -0600

    test: Recipes controller will return an recipes by area

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,20 +2,20 @@ class Api::V1::RecipesController < ApplicationController
   def random_find
     render json: RecipesSerializer.new(RecipesFacade.find_random_recipe)
   end
+  
+  def id_find
+    begin 
+      render json: RecipesSerializer.new(RecipesFacade.find_recipe_by_id(params[:id]))
+    rescue NoMethodError 
+      render json: {text: "Hahah caught you! Sneaky Sneak, can't fool us", status: 404}
+    end 
+  end
 
   def name_find
     begin 
       render json: RecipesSerializer.new(RecipesFacade.find_recipes_by_name(params[:q]))
     rescue NoMethodError
       render json: {text: "Search produced no results", status: 404}
-    end 
-  end
-
-  def id_find
-    begin 
-      render json: RecipesSerializer.new(RecipesFacade.find_recipe_by_id(params[:id]))
-    rescue NoMethodError 
-      render json: {text: "Hahah caught you! Sneaky Sneak, can't fool us", status: 404}
     end 
   end
 
--- spec/requests/api/v1/recipes/recipe_request_spec.rb
+++ spec/requests/api/v1/recipes/recipe_request_spec.rb
@@ -91,6 +91,23 @@ describe 'recipe request' do
       expect(recipe[:attributes][:ingredients]).to be_a(Hash)  
     end
   end 
+  
+  it '#area_find', :vcr do
+    get '/api/v1/recipes/area?q=British'
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)[:data]
+    parsed.each do |recipe|
+      expect(recipe).to have_key(:type)
+      expect(recipe).to have_key(:id)
+      expect(recipe).to have_key(:attributes)
+      expect(recipe[:attributes]).to have_key(:area)
+      expect(recipe[:attributes]).to have_key(:name)
+      expect(recipe[:attributes]).to have_key(:category)
+      expect(recipe[:attributes]).to have_key(:ingredients)
+      expect(recipe[:attributes]).to have_key(:instructions)
+      expect(recipe[:attributes][:ingredients]).to be_a(Hash)  
+    end
+  end 
 
   xit 'returns a status 404 if query isnt valid', :vcr do 
     get '/api/v1/recipes/search?q=foobar'
